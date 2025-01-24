1afe4cacd67a94b4120d7539fcf58472edf47d20
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-01-24 18:51:10 +0100

    fix: move index.html in the root directory
    style: rename assests to assets

--- app/src/main.js
+++ app/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./routes/routes";
-import "./assests/css/style.css";
+import "./assets/css/style.css"; // Ensure this path is correct
 
 const app = createApp(App);
 const pinia = createPinia();
