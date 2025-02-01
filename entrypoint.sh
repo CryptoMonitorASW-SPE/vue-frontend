0a56eba598ce63012029917b716c9a38e4a2bec4
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-02-01 02:32:48 +0100

    feat: add user management service to Nginx configuration with CORS support

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@
 
 envsubst '\
     \${AUTHENTICATION_SERVICE_NAME} \${AUTHENTICATION_SERVICE_PORT} \
-    ${CRYPTOMARKET_SERVICE_NAME} \${CRYPTOMARKET_SERVICE_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/nginx.conf
+    ${CRYPTOMARKET_SERVICE_NAME} \${CRYPTOMARKET_SERVICE_PORT}
+    \${USER_MANAGEMENT_SERVICE_NAME} \${USER_MANAGEMENT_SERVICE_PORT}'< /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Nginx
 exec "$@"
